b26fa6a893860f3a8e02b96ed41e0bf4ee6d06fe
GazJack <greta.jackaite@gmail.com>
2025-03-03 10:17:40 +0200

    36ps. Mo. tictactoe CI-CD pipeline

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,50 @@
+describe('Tic Tac Toe testing', () => {
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:5173')
+  });
+
+  it('Should board be empty and X first player', () => {
+    cy.get('[data-testid="status"]').should('contain', 'Next player: X')
+    cy.get('.square').should('have.length', 9)
+    cy.get('.square').each($square => {
+      cy.wrap($square).should('be.empty')
+    });
+  });
+
+  it('Should players move alternately', () => {
+    cy.get('[data-testid="square-0"]').click();
+    cy.get('[data-testid="square-0"]').should('contain', 'X');
+    cy.get('[data-testid="status"]').should('contain', 'Next player: O');
+
+    cy.get('[data-testid="square-5"]').click();
+    cy.get('[data-testid="square-5"]').should('contain', 'O');
+    cy.get('[data-testid="status"]').should('contain', 'Next player: X');
+  });
+  
+  it('Should declare winner', () => {
+    cy.get('[data-testid="square-0"]').click(); //X
+    cy.get('[data-testid="square-1"]').click(); //0
+    cy.get('[data-testid="square-3"]').click(); //X
+    cy.get('[data-testid="square-4"]').click(); //0
+    cy.get('[data-testid="square-6"]').click(); //X
+
+  cy.get('[data-testid="status"]').should('contain', 'Winner: X');
+});
+
+  it('Should game reset', () => {
+    cy.get('[data-testid="square-0"]').click(); //X
+    cy.get('[data-testid="square-1"]').click(); //0
+    cy.get('[data-testid="square-3"]').click(); //X
+    cy.get('[data-testid="square-4"]').click(); //0
+    cy.get('[data-testid="square-6"]').click(); //X
+
+  cy.get('[data-testid="status"]').should('contain', 'Winner: X');
+
+    cy.get('[data-testid="reset-button"]').click();
+    
+    cy.get('[data-testid="status"]').should('contain', 'Next player: X')
+    cy.get('.square').each($square => {
+      cy.wrap($square).should('be.empty');
+    });
+  });
+});
